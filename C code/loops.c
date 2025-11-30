c6aabdc080fb94658ff7c05782723350e4b639cd
sMacks28 <nidensmax0@gmail.com>
2025-11-30 20:39:15 +0100

    added sizeof to bubble sort with bugs

--- C code/loops.c
+++ C code/loops.c
@@ -3,11 +3,22 @@
 int main()
 {
     //sorting algorithm
-    int x, i, j, temp, times;
-    int list[] = {4, 1, 2, 8, 3, 14, 10, 9};
-    for (i = 0; i<7; i++)
+    int list[] = {2, 4, 8, 1, 2, 5, 8, 3, 2, 3, 5, 7, 7, 4, 2, 1};
+    int x, i, j, temp, times, k, a, y, size;
+
+
+    //Benutze sifeof operator um die Länge des arrays zu bestimmen
+    //Der sizeof operator gibt die anzahl an bytes eines datentyps aus bspw:
+    //int 2 oder 4 bytes (32 oder 64 bit)
+    //float 4 bytes double 8 bytes
+    //char 1 byte
+    //dividiere dann durch ein element des arrays um die größe nicht in byte sondern in elementen zu erhalten
+    
+    size = sizeof(list)/sizeof(list[0]); 
+
+    for (i = 0; i<size; i++)
     {
-        for (j = 0; j<(7-i); j++)
+        for (j = 0; j<(size-i); j++)
         {
         x = list[j] - list[j+1]; //Wenn das i-te Element größer als das i+1-te Element ist müssen die plätze getauscht werden
         if (list[j] > list[j+1])
@@ -21,11 +32,11 @@ int main()
         times++;
     }
 
-    int k, a;
     for (k = 0; k<7; k++)
     {
         a = list[k];
         printf("%d\t", a);
     }
+    printf("Die Größe der Liste ist %d", size);
     return 0;
 }
